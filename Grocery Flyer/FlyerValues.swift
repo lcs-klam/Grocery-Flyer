4adb7d218dc3531972f87b362201d01f5a716d48
Kevin Lam <klam@lcs.on.ca>
2025-02-05 10:53:38 -0500

    Added prices, quantities, and measurements into the data model

--- Grocery Flyer/FlyerValues.swift
+++ Grocery Flyer/FlyerValues.swift
@@ -21,6 +21,9 @@ struct FoodItem: Identifiable {
     let id = UUID()
     let name: String
     let image: String
+    let price: String
+    let quantity: Int
+    let measurement: String
 }
 
 let thisWeeksFlyer = Flyer(
@@ -32,11 +35,17 @@ let thisWeeksFlyer = Flyer(
             items: [
                 FoodItem(
                     name: "Sirloin Tip Oven Roasts",
-                    image: "sirloin-tips"
+                    image: "sirloin-tips",
+                    price: "$3",
+                    quantity: 2,
+                    measurement: "lbs"
                 ),
                 FoodItem(
                     name: "Porkloin Back Ribs",
-                    image: "porkloin-back-ribs"
+                    image: "porkloin-back-ribs",
+                    price: "$3",
+                    quantity: 2,
+                    measurement: "lbs"
                 ),
             ]
         ),
@@ -46,15 +55,24 @@ let thisWeeksFlyer = Flyer(
             items: [
                 FoodItem(
                     name: "Wild Pacific Halibut Fillets",
-                    image: "wild-pacific-halibut-fillets"
+                    image: "wild-pacific-halibut-fillets",
+                    price: "$3",
+                    quantity: 2,
+                    measurement: "lbs"
                 ),
                 FoodItem(
                     name: "Wild Sockeye Salmon Fillets",
-                    image: "wild-sockeye-salmon-fillets"
+                    image: "wild-sockeye-salmon-fillets",
+                    price: "$3",
+                    quantity: 2,
+                    measurement: "lbs"
                 ),
                 FoodItem(
                     name: "Clam Chowder",
-                    image: "clam-chowder"
+                    image: "clam-chowder",
+                    price: "$3",
+                    quantity: 2,
+                    measurement: "lbs"
                 ),
             ]
         ),
