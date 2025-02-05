cf3a29ccc0dd4d3a9eab40564d18f4e19f91a778
Kevin Lam <klam@lcs.on.ca>
2025-02-05 09:45:07 -0500

    Initial Commit

---
+++ Grocery Flyer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grocery Flyer
+//
+//  Created by Kevin Lam on 5/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grocery Flyer/Grocery_FlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_FlyerApp.swift
+//  Grocery Flyer
+//
+//  Created by Kevin Lam on 5/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_FlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
