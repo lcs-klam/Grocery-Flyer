c2579228afe4ef5da6441704790ea62acc75c1ab
Kevin Lam <klam@lcs.on.ca>
2025-02-07 11:43:26 -0500

    Save 1 for work on the navigationview.

--- Grocery Flyer/Views/NavigationView.swift
+++ Grocery Flyer/Views/NavigationView.swift
@@ -7,5 +7,17 @@
 
 import SwiftUI
 
+struct NavigationView: View {
+    let departmentToShow: Department
+    
+    var body: some View {
+        List(departmentToShow.items) { currentItem in
+            VStack(alignment: .leading) {
+                Text(currentItem.name)
+                    .font(.headline)
+                
+                Text("\(currentItem.price) per \(currentItem.quantity) \(currentItem.measurement)")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
 
 
